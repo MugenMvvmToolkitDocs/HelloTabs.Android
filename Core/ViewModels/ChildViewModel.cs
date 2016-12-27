514b6cb60890e77c0063adfeb6ae33a6532907d1
aedampir@gmail.com <aedampir@gmail.com>
2016-12-27 18:59:28 +0700

    Initial commit

---
+++ Core/ViewModels/ChildViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MugenMvvmToolkit.ViewModels;
+
+namespace Core.ViewModels
+{
+    public class ChildViewModel : ViewModelBase
+    {
+        public int Number { get; private set; }
+
+        public void Init(int number)
+        {
+            Number = number;
+        }
+    }
+
+}
---
+++ Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using MugenMvvmToolkit.ViewModels;
+
+namespace Core.ViewModels
+{
+    public class MainViewModel : MultiViewModel
+    {
+        #region Overrides of ViewModelBase
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            for (var i = 0; i < 5; i++)
+            {
+                var vm = GetViewModel<ChildViewModel>();
+                vm.Init(i + 1);
+                AddViewModel(vm, false);
+            }
+
+            // set selected vm
+            SelectedItem = ItemsSource[0];
+        }
+
+        #endregion
+    }
+}
